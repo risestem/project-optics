c6e3fa703a97ce13edd180843ee2ccc0e24d5b71
Joshua Luo <74257863+cyber-yosh@users.noreply.github.com>
2025-07-27 00:05:19 -0400

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,20 @@
 from fastapi import FastAPI, Request, HTTPException
 from utils.llm import call_llm
-from utils.helpers import animate_scene
+from utils.helpers import cloud_render
+import uvicorn
+
+app = FastAPI()
+
+@app.get('/optics/{session_id}/{prompt}')
+def get_optics(session_id : int, prompt: str):
+    call = call_llm(prompt, {})
+    response = cloud_render(call, session_id)
+    return response
 
 if __name__ == "__main__":
     response = call_llm("dfs on a graph")
-    animate_scene(response)
+    cloud_render(response)
+    uivicorn.run(app, host="0.0.0", port=8000)
 
-
-def 
+ 
 
